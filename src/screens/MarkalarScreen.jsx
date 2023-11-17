24f2595f5ff20ad160e2fbe84ed315c0f4639d97
Ali Berkay Celik <aliberkaycelik@hotmail.com>
2023-11-17 23:47:53 +0300

    first commit

---
+++ src/screens/MarkalarScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MarkalarScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default MarkalarScreen
+
+const styles = StyleSheet.create({})
